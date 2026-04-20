a7bc050257e23e71f20c7228ccf51ecbb11041de
nnn23 <nika.pykh@gmail.com>
2026-04-21 02:56:55 +0300

    Добавлен main.cpp, проект готов к сборке

--- main.cpp
+++ main.cpp
@@ -1,41 +1,17 @@
+#include "turingmachine.h"
 #include <QApplication>
-#include <QWidget>
-#include <QVBoxLayout>
-#include <QLabel>
-#include <QLineEdit>
-#include <QPushButton>
-#include <QMessageBox>
 
-int main(int argc, char *argv[]) {
-    QApplication app(argc, argv);
+int main(int argc, char *argv[])
+{
+    // Создаем объект приложения
+    QApplication a(argc, argv);
 
-    // Создаем главное окно
-    QWidget window;
-    window.setWindowTitle("Эмулятор Машины Тьюринга");
+    // Создаем твое окно
+    TuringMachine w;
 
-    // Создаем элементы
-    QLabel *title = new QLabel("Введите алфавиты для начала работы");
-    QLineEdit *alphabetInput = new QLineEdit();
-    alphabetInput->setPlaceholderText("Например: abc");
+    // Показываем его
+    w.show();
 
-    QPushButton *nextBtn = new QPushButton("Далее");
-
-    // Расставляем их вертикально
-    QVBoxLayout *layout = new QVBoxLayout;
-    layout->addWidget(title);
-    layout->addWidget(alphabetInput);
-    layout->addWidget(nextBtn);
-
-    window.setLayout(layout);
-
-    // Оживляем кнопку: при нажатии покажем сообщение
-    QObject::connect(nextBtn, &QPushButton::clicked, [&]() {
-        QString text = alphabetInput->text();
-        QMessageBox::information(&window, "Готово!", "Алфавит принят: " + text);
-    });
-
-    window.resize(300, 200);
-    window.show();
-
-    return app.exec();
+    // Запускаем цикл обработки событий (чтобы кнопки нажимались)
+    return a.exec();
 }
